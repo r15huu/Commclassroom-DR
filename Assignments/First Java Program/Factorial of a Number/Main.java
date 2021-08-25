8c3c84a379fa7ad687e3057ed0cf30c2965f4d6d
Dipankar <dipankarroy1023@gmail.com>
2021-08-25 17:48:00 +0530

    Added more assignments

---
+++ Assignments/First Java Program/Factorial of a Number/Main.java
@@ -0,0 +1,24 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        int num = input.nextInt();
+        int x = num;
+        int y = 1;
+
+        if (num == 0){
+            System.out.println(1);
+        } else {
+            for (int i = 0; i<x; i++){
+                y *= num;
+                num--;
+            }
+        }
+
+        System.out.println(y);
+    }
+}
---
+++ Assignments/First Java Program/Greetings/Main.java
@@ -0,0 +1,14 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+
+	Scanner input = new Scanner(System.in);
+	String name = input.next().trim();
+	System.out.println("Hey there " + name + "!");
+
+    }
+}
