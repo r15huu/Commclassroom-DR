56043ef41b656759abda5302a746bae840e9f9ed
Dipankar <dipankarroy1023@gmail.com>
2021-08-27 09:45:01 +0530

    added assignment

---
+++ Assignments/First Java Program/Calculate the Value/Main.java
@@ -0,0 +1,31 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+        System.out.print("Enter the Operator: ");
+        char op = input.next().trim().charAt(0);
+        System.out.print("Enter first number: ");
+	    int num1 = input.nextInt();
+        System.out.print("Enter Second Number: ");
+	    int num2 = input.nextInt();
+
+	    if( op == '+'){
+            System.out.println("Answer: " + (num1 + num2));
+        } else if ( op == '-') {
+            System.out.println("Answer: " + (num1 - num2));
+        } else if (op == '/') {
+            System.out.println("Answer: " + (num1/num2));
+        } else if (op == '*') {
+            System.out.println("Answer: " + (num1 * num2));
+        } else {
+            System.out.println("Enter valid Operator");
+        }
+
+
+
+    }
+}
